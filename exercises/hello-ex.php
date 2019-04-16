09943240730d028a918f3428e63359c68c0cc64f
Lucas Kohorst <kohorstlucas@gmail.com>
2019-04-16 10:58:38 -0400

    Database

---
+++ exercises/hello-ex.php
@@ -0,0 +1,15 @@
+<!--
+FILE: hello-ex.php
+DESC: A simple exercise page about gettting started in Java.
+AUTH: Mikayla Wishart
+DATE: 04/06/2019
+-->
+
+<?php
+
+   $pagename = "Hello";
+   $pagenumber = 4;
+
+   include "../assets/inc/content.php";
+
+?>
