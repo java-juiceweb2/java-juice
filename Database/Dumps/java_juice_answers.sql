965946aca22b5dbddab424ba259ed7bc7c7b54c8
ace7270 <47132438+ace7270@users.noreply.github.com>
2019-04-18 19:43:34 -0400

    Update java_juice_answers.sql

--- Database/Dumps/java_juice_answers.sql
+++ Database/Dumps/java_juice_answers.sql
@@ -33,39 +33,40 @@ CREATE TABLE `java_juice_answers` (
   `A` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `B` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `C` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `D` varchar(200) COLLATE utf8_unicode_ci NOT NULL
+  `D` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `correctAnswer` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `java_juice_answers`
 --
 
-INSERT INTO `java_juice_answers` (`questionID`, `A`, `B`, `C`, `D`) VALUES
-(1, 'public static void main(String[] args) {}', 'public void static main(String[] args) []', 'private static void main(String[] args) {}', 'public static void main(String args[]) []'),
-(2, 'System.out.println()', 'System.out.printf()', 'System.err.println()', 'print()'),
-(3, 'javac FILE', 'java compile FILE', 'java FILE', 'compile FILE'),
-(4, 'java run FILE', 'java FILE', 'run FILE', 'run java FILE'),
-(5, '4', '6', '8', 'They are all Objects'),
-(6, '0', '-4', '4.65', '10'),
-(7, 'int num = 8', 'num = 8', '8', '8 = int'),
-(8, 'float', 'byte', 'char', 'long'),
-(9, 'String str = Hello', 'String str = \"Hello\"', 'string str = \"Hello\"', 'string str = hello'),
-(10, 'Integer to Double', 'Integer to Long', 'Double to Integer', 'Integer to Float'),
-(11, 'double.intValue()', 'double.IntegerValue()', 'double.int()', 'double.value()'),
-(12, 'false', 'True', 'Hello', '4'),
-(13, 'true', 'false', 'Hello', 'error'),
-(14, '&', 'OR', '|', '||'),
-(15, 'not equal', '!=', 'not =', '==='),
-(16, 'Hello', 'error: infinte loop', 'nothing', 'true'),
-(17, 'while loop', 'if statment', 'for loop', 'do while loop'),
-(18, 'incrementor, expression, increment', 'incrementor, expression', 'incrementor, expression, code', 'incrementor, value, increment'),
-(19, 'exit', 'break', 'leave', 'done'),
-(20, 'A data type', 'An Object', 'Something that can be called to execute code', 'A variable'),
-(21, 'once', 'As much as you want', '3 times', 'never'),
-(22, 'It is not a method', 'It does not return anything', 'It is unusable', 'It holds no values'),
-(23, 'Gets command line input', 'Just looks cool', 'It is the standard', 'Intializes java'),
-(24, 'Exits the method', 'Sets a variable', 'Exits out of the method with the variable declared after it', 'nothing'),
-(25, 'Nothing will happen', 'Your code will still execute', 'Compiler error', 'Computer Crash');
+INSERT INTO `java_juice_answers` (`questionID`, `A`, `B`, `C`, `D`, `correctAnswer`) VALUES
+(1, 'public static void main(String[] args) {}', 'public void static main(String[] args) []', 'private static void main(String[] args) {}', 'public static void main(String args[]) []', 'A'),
+(2, 'System.out.println()', 'System.out.printf()', 'System.err.println()', 'print()', 'D'),
+(3, 'javac FILE', 'java compile FILE', 'java FILE', 'compile FILE', 'A'),
+(4, 'java run FILE', 'java FILE', 'run FILE', 'run java FILE', 'B'),
+(5, '4', '6', '8', 'They are all Objects', 'C'),
+(6, '0', '-4', '4.65', '10', 'C'),
+(7, 'int num = 8', 'num = 8', '8', '8 = int', 'A'),
+(8, 'float', 'byte', 'char', 'long', 'D'),
+(9, 'String str = Hello', 'String str = \"Hello\"', 'string str = \"Hello\"', 'string str = hello', 'B'),
+(10, 'Integer to Double', 'Integer to Long', 'Double to Integer', 'Integer to Float', 'C'),
+(11, 'double.intValue()', 'double.IntegerValue()', 'double.int()', 'double.value()', 'A'),
+(12, 'false', 'True', 'Hello', '4', 'A'),
+(13, 'true', 'false', 'Hello', 'error', 'B'),
+(14, '&', 'OR', '|', '||', 'D'),
+(15, 'not equal', '!=', 'not =', '===', 'B'),
+(16, 'Hello', 'error: infinte loop', 'nothing', 'true', 'B'),
+(17, 'while loop', 'if statment', 'for loop', 'do while loop', 'D'),
+(18, 'incrementor, expression, increment', 'incrementor, expression', 'incrementor, expression, code', 'incrementor, value, increment', 'A'),
+(19, 'exit', 'break', 'leave', 'done', 'B'),
+(20, 'A data type', 'An Object', 'Something that can be called to execute code', 'A variable', 'C'),
+(21, 'once', 'As much as you want', '3 times', 'never', 'B'),
+(22, 'It is not a method', 'It does not return anything', 'It is unusable', 'It holds no values', 'B'),
+(23, 'Gets command line input', 'Just looks cool', 'It is the standard', 'Intializes java', 'A'),
+(24, 'Exits the method', 'Sets a variable', 'Exits out of the method with the variable declared after it', 'nothing', 'C'),
+(25, 'Nothing will happen', 'Your code will still execute', 'Compiler error', 'Computer Crash', 'A');
 
 --
 -- Indexes for dumped tables
