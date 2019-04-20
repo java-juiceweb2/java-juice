b056130acf1e290aa3038253f9f9e90ef31ca5f2
Aaron <ace7270@rit.edu>
2019-04-20 03:34:03 -0400

    Update quizProcess.php

--- exercises/quizProcess.php
+++ exercises/quizProcess.php
@@ -21,14 +21,27 @@ Last Updated: 4/18/19
 <?php
     echo "<div id='results-container'>";
     $numCorrect = 0;
-    $correctLetter = null;
-    $question = null;
-    $correctAnswer = null;
-    $userLetter = null;
-    $userAnswer = null;
+
+    $stmt=$CONN->prepare("insert into java_juice_quiz_results (username, question1, question2, question3,	question4, question5, question6, question7, question8, question9, question10) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        $stmt->bind_param("sssssssssss", $_SESSION['user_name'], $answers[0], $answers[1], $answers[2], $answers[3], $answers[4], $answers[5], $answers[6], $answers[7], $answers[8], $answers[9]);
+        $stmt->execute();
+        $stmt->close();
 
 
+    // calculate user's score out of 10
+    for ($i = 0; $i < sizeof($_SESSION['chosenAnswers']); $i++) {
+        
+        $correctLetter = $_SESSION['chosenAnswers'][$i]['correctAnswer'];
+        $userLetter = $answers[$i];        
+        if ($userLetter == $correctLetter) {
+            $numCorrect++;
+        }
+        
+    }
 
+    // display results
+    echo "<h3>Total Correct: ".$numCorrect."/10</h3><br>";
     for ($i = 0; $i < sizeof($_SESSION['chosenAnswers']); $i++) {
         $correctLetter = $_SESSION['chosenAnswers'][$i]['correctAnswer'];
         $question = $_SESSION['chosenQuestions'][$i];
@@ -36,18 +49,13 @@ Last Updated: 4/18/19
         $userLetter = $answers[$i];
         $userAnswer = $_SESSION['chosenAnswers'][$i][$userLetter];
         
-        if ($userLetter == $correctLetter) {
-            $numCorrect++;
-        }
-    }
-    echo "<p>Total Correct: ".$numCorrect."/10</p>";
-    for ($i = 0; $i < sizeof($_SESSION['chosenAnswers']); $i++) {
-        
         echo "<p>Question ".($i + 1).") ".$question."</p>
         <p>Correct Answer: ".$correctLetter.") ".$correctAnswer."</p>
         <p>Your Answer: ".$userLetter.") ".$userAnswer."</p><br>";
     }
     echo "</div>";
+
+    
 
     include "../assets/inc/footer.php";
 ?>
