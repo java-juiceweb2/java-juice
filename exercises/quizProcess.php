81f6cf47c5e78c2fec4439de10cf72466696efa8
Aaron <ace7270@rit.edu>
2019-04-19 21:59:14 -0400

    Update quizProcess.php

--- exercises/quizProcess.php
+++ exercises/quizProcess.php
@@ -20,15 +20,30 @@ Last Updated: 4/18/19
 
 <?php
     echo "<div id='results-container'>";
+    $numCorrect = 0;
+    $correctLetter = null;
+    $question = null;
+    $correctAnswer = null;
+    $userLetter = null;
+    $userAnswer = null;
+
+
+
     for ($i = 0; $i < sizeof($_SESSION['chosenAnswers']); $i++) {
-        
         $correctLetter = $_SESSION['chosenAnswers'][$i]['correctAnswer'];
         $question = $_SESSION['chosenQuestions'][$i];
         $correctAnswer = $_SESSION['chosenAnswers'][$i][$correctLetter];
         $userLetter = $answers[$i];
         $userAnswer = $_SESSION['chosenAnswers'][$i][$userLetter];
         
-        echo "<p>Question ".($i + 1).") ".$question."</p>
+        if ($userLetter == $correctLetter) {
+            $numCorrect++
+        }
+    }
+    for ($i = 0; $i < sizeof($_SESSION['chosenAnswers']); $i++) {
+        
+        echo "<p>Total Correct: ".$numCorrect."/10</p>
+        <p>Question ".($i + 1).") ".$question."</p>
         <p>Correct Answer: ".$correctLetter.") ".$correctAnswer."</p>
         <p>Your Answer: ".$userLetter.") ".$userAnswer."</p><br>";
     }
